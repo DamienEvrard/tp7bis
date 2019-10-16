1885850007d54938f98246dc1b1ee498d194a1a9
DamienEvrard <damien.31861@laposte.net>
2019-10-16 15:59:09 +0200

    initialisation + debut

--- src/main/java/invoice/DAO.java
+++ src/main/java/invoice/DAO.java
@@ -10,168 +10,217 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.sql.DataSource;
+import invoice.CustomerEntity;
+import java.util.ArrayList;
 
 public class DAO {
 
-	private final DataSource myDataSource;
+    private final DataSource myDataSource;
 
-	/**
-	 *
-	 * @param dataSource la source de données à utiliser
-	 */
-	public DAO(DataSource dataSource) {
-		this.myDataSource = dataSource;
-	}
+    /**
+     *
+     * @param dataSource la source de données à utiliser
+     */
+    public DAO(DataSource dataSource) {
+        this.myDataSource = dataSource;
+    }
 
-	/**
-	 * Renvoie le chiffre d'affaire d'un client (somme du montant de ses factures)
-	 *
-	 * @param id la clé du client à chercher
-	 * @return le chiffre d'affaire de ce client ou 0 si pas trouvé
-	 * @throws SQLException
-	 */
-	public float totalForCustomer(int id) throws SQLException {
-		String sql = "SELECT SUM(Total) AS Amount FROM Invoice WHERE CustomerID = ?";
-		float result = 0;
-		try (Connection connection = myDataSource.getConnection();
-			PreparedStatement statement = connection.prepareStatement(sql)) {
-			statement.setInt(1, id); // On fixe le 1° paramètre de la requête
-			try (ResultSet resultSet = statement.executeQuery()) {
-				if (resultSet.next()) {
-					result = resultSet.getFloat("Amount");
-				}
-			}
-		}
-		return result;
-	}
+    /**
+     * Renvoie le chiffre d'affaire d'un client (somme du montant de ses
+     * factures)
+     *
+     * @param id la clé du client à chercher
+     * @return le chiffre d'affaire de ce client ou 0 si pas trouvé
+     * @throws SQLException
+     */
+    public float totalForCustomer(int id) throws SQLException {
+        String sql = "SELECT SUM(Total) AS Amount FROM Invoice WHERE CustomerID = ?";
+        float result = 0;
+        try (Connection connection = myDataSource.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id); // On fixe le 1° paramètre de la requête
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    result = resultSet.getFloat("Amount");
+                }
+            }
+        }
+        return result;
+    }
 
-	/**
-	 * Renvoie le nom d'un client à partir de son ID
-	 *
-	 * @param id la clé du client à chercher
-	 * @return le nom du client (LastName) ou null si pas trouvé
-	 * @throws SQLException
-	 */
-	public String nameOfCustomer(int id) throws SQLException {
-		String sql = "SELECT LastName FROM Customer WHERE ID = ?";
-		String result = null;
-		try (Connection connection = myDataSource.getConnection();
-			PreparedStatement statement = connection.prepareStatement(sql)) {
-			statement.setInt(1, id);
-			try (ResultSet resultSet = statement.executeQuery()) {
-				if (resultSet.next()) {
-					result = resultSet.getString("LastName");
-				}
-			}
-		}
-		return result;
-	}
+    /**
+     * Renvoie le nom d'un client à partir de son ID
+     *
+     * @param id la clé du client à chercher
+     * @return le nom du client (LastName) ou null si pas trouvé
+     * @throws SQLException
+     */
+    public String nameOfCustomer(int id) throws SQLException {
+        String sql = "SELECT LastName FROM Customer WHERE ID = ?";
+        String result = null;
+        try (Connection connection = myDataSource.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    result = resultSet.getString("LastName");
+                }
+            }
+        }
+        return result;
+    }
 
-	/**
-	 * Transaction permettant de créer une facture pour un client
-	 *
-	 * @param customer Le client
-	 * @param productIDs tableau des numéros de produits à créer dans la facture
-	 * @param quantities tableau des quantités de produits à facturer faux sinon Les deux tableaux doivent avoir la même
-	 * taille
-	 * @throws java.lang.Exception si la transaction a échoué
-	 */
-	public void createInvoice(CustomerEntity customer, int[] productIDs, int[] quantities)
-		throws Exception {
-		throw new UnsupportedOperationException("Pas encore implémenté");
-	}
+    /**
+     * insert dans la table Item les produits et la quantité commandé par le
+     * client
+     *
+     * @param customer
+     * @param productIDs
+     * @param productIDs
+     * @param quantities
+     * @throws Exception
+     */
+    public void createInvoice(CustomerEntity customer, int[] productIDs, int[] quantities) throws Exception {
+        //__________________________________________________________________________________________________________________________________________
 
-	/**
-	 *
-	 * @return le nombre d'enregistrements dans la table CUSTOMER
-	 * @throws SQLException
-	 */
-	public int numberOfCustomers() throws SQLException {
-		int result = 0;
+        String sqlPrixProd = "SELECT Price AS Prix FROM Product where ID=?";
+        String sqlInvoice = "Insert into Invoice(CustomerID) values(?)";
+        String sqlItem = "Insert into Item(InvoiceID ,ProductID ,Quantity ,Cost) values(?,?,?,?,?)";
 
-		String sql = "SELECT COUNT(*) AS NUMBER FROM Customer";
-		try (Connection connection = myDataSource.getConnection();
-			Statement stmt = connection.createStatement()) {
-			ResultSet rs = stmt.executeQuery(sql);
-			if (rs.next()) {
-				result = rs.getInt("NUMBER");
-			}
-		}
-		return result;
-	}
+        int[] PriceP = new int[productIDs.length];
+        try (Connection connection = myDataSource.getConnection()) {
+            connection.setAutoCommit(false);
 
-	/**
-	 *
-	 * @param customerId la clé du client à recherche
-	 * @return le nombre de bons de commande pour ce client (table PURCHASE_ORDER)
-	 * @throws SQLException
-	 */
-	public int numberOfInvoicesForCustomer(int customerId) throws SQLException {
-		int result = 0;
+            try (PreparedStatement stmt = connection.prepareStatement(sqlPrixProd)) {
+                for (int i = 0; i < productIDs.length; i++) {
+                    stmt.setInt(1, productIDs[i]);
+                    ResultSet rs = stmt.executeQuery(sqlPrixProd);
+                    if (rs.next()) {
+                        PriceP[i] = rs.getInt("Price");
+                    }
+                }
+            } catch (Exception e) {
+                throw new Exception("produit introuvable");
+            }
 
-		String sql = "SELECT COUNT(*) AS NUMBER FROM Invoice WHERE CustomerID = ?";
+            try (PreparedStatement stmt = connection.prepareStatement(sqlInvoice); ResultSet idI = stmt.getGeneratedKeys()) {
+                stmt.setInt(1, customer.getCustomerId());
+                ResultSet rs = stmt.executeQuery(sqlInvoice);
+                if (rs.next()) {
+                    for (int i = 0; i < productIDs.length; i++) {
+                        try (PreparedStatement stmt2 = connection.prepareStatement(sqlItem)) {
+                            stmt.setInt(1, idI.getInt(1));
+                            stmt.setInt(2, productIDs[i]);
+                            stmt.setInt(3, quantities[i]);
+                            stmt.setInt(4, quantities[i] * PriceP[i]);
+                            ResultSet rsItem = stmt.executeQuery(sqlItem);
 
-		try (Connection connection = myDataSource.getConnection();
-			PreparedStatement stmt = connection.prepareStatement(sql)) {
-			stmt.setInt(1, customerId);
-			ResultSet rs = stmt.executeQuery();
-			if (rs.next()) {
-				result = rs.getInt("NUMBER");
-			}
-		}
-		return result;
-	}
+                        }
+                    }
+                }
+                connection.commit();
+            } catch (Exception e) {
+                connection.rollback();
+                throw new Exception("client introuvable");
+            } finally {
+                connection.setAutoCommit(true);
+            }
+        }
+    }
 
-	/**
-	 * Trouver un Customer à partir de sa clé
-	 *
-	 * @param customedID la clé du CUSTOMER à rechercher
-	 * @return l'enregistrement correspondant dans la table CUSTOMER, ou null si pas trouvé
-	 * @throws SQLException
-	 */
-	CustomerEntity findCustomer(int customerID) throws SQLException {
-		CustomerEntity result = null;
+    /**
+     *
+     * @return le nombre d'enregistrements dans la table CUSTOMER
+     * @throws SQLException
+     */
+    public int numberOfCustomers() throws SQLException {
+        int result = 0;
 
-		String sql = "SELECT * FROM Customer WHERE ID = ?";
-		try (Connection connection = myDataSource.getConnection();
-			PreparedStatement stmt = connection.prepareStatement(sql)) {
-			stmt.setInt(1, customerID);
+        String sql = "SELECT COUNT(*) AS NUMBER FROM Customer";
+        try (Connection connection = myDataSource.getConnection();
+                Statement stmt = connection.createStatement()) {
+            ResultSet rs = stmt.executeQuery(sql);
+            if (rs.next()) {
+                result = rs.getInt("NUMBER");
+            }
+        }
+        return result;
+    }
 
-			ResultSet rs = stmt.executeQuery();
-			if (rs.next()) {
-				String name = rs.getString("FirstName");
-				String address = rs.getString("Street");
-				result = new CustomerEntity(customerID, name, address);
-			}
-		}
-		return result;
-	}
+    /**
+     *
+     * @param customerId la clé du client à recherche
+     * @return le nombre de bons de commande pour ce client (table
+     * PURCHASE_ORDER)
+     * @throws SQLException
+     */
+    public int numberOfInvoicesForCustomer(int customerId) throws SQLException {
+        int result = 0;
 
-	/**
-	 * Liste des clients localisés dans un état des USA
-	 *
-	 * @param state l'état à rechercher (2 caractères)
-	 * @return la liste des clients habitant dans cet état
-	 * @throws SQLException
-	 */
-	List<CustomerEntity> customersInCity(String city) throws SQLException {
-		List<CustomerEntity> result = new LinkedList<>();
+        String sql = "SELECT COUNT(*) AS NUMBER FROM Invoice WHERE CustomerID = ?";
 
-		String sql = "SELECT * FROM Customer WHERE City = ?";
-		try (Connection connection = myDataSource.getConnection();
-			PreparedStatement stmt = connection.prepareStatement(sql)) {
-			stmt.setString(1, city);
-			try (ResultSet rs = stmt.executeQuery()) {
-				while (rs.next()) {
-					int id = rs.getInt("ID");
-					String name = rs.getString("FirstName");
-					String address = rs.getString("Street");
-					CustomerEntity c = new CustomerEntity(id, name, address);
-					result.add(c);
-				}
-			}
-		}
+        try (Connection connection = myDataSource.getConnection();
+                PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, customerId);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                result = rs.getInt("NUMBER");
+            }
+        }
+        return result;
+    }
 
-		return result;
-	}
+    /**
+     * Trouver un Customer à partir de sa clé
+     *
+     * @param customedID la clé du CUSTOMER à rechercher
+     * @return l'enregistrement correspondant dans la table CUSTOMER, ou null si
+     * pas trouvé
+     * @throws SQLException
+     */
+    CustomerEntity findCustomer(int customerID) throws SQLException {
+        CustomerEntity result = null;
+
+        String sql = "SELECT * FROM Customer WHERE ID = ?";
+        try (Connection connection = myDataSource.getConnection();
+                PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, customerID);
+
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                String name = rs.getString("FirstName");
+                String address = rs.getString("Street");
+                result = new CustomerEntity(customerID, name, address);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Liste des clients localisés dans un état des USA
+     *
+     * @param state l'état à rechercher (2 caractères)
+     * @return la liste des clients habitant dans cet état
+     * @throws SQLException
+     */
+    List<CustomerEntity> customersInCity(String city) throws SQLException {
+        List<CustomerEntity> result = new LinkedList<>();
+
+        String sql = "SELECT * FROM Customer WHERE City = ?";
+        try (Connection connection = myDataSource.getConnection();
+                PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, city);
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    int id = rs.getInt("ID");
+                    String name = rs.getString("FirstName");
+                    String address = rs.getString("Street");
+                    CustomerEntity c = new CustomerEntity(id, name, address);
+                    result.add(c);
+                }
+            }
+        }
+
+        return result;
+    }
 }
